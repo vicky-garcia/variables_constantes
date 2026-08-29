ff8334c3d9200e238f155b7cd9fc3c8be21512db
vicky-garcia <ve.garcia@unitecnica.net>
2026-08-28 20:09:40 -0500

    commit inicial

---
+++ constantes.js
@@ -0,0 +1,4 @@
+const names = "Santiago";
+const age = 20;
+
+console.log(`Hola, me llamo ${names} y tengo ${age} años.`);
---
+++ variables.js
@@ -0,0 +1,17 @@
+/*let name; // Solo es la declaración de la variable, no se le asigna ningún valor.
+let age = 23; // Se declara la variable age y se le asigna el valor 23.
+
+name = "Victoria"; // Se le asigna el valor "Victoria" a la variable name.
+
+console.log(`Hola, me llamo ${name} y tengo ${age} años.`); */
+
+const a = 10;
+const b = 5;
+const c = 13;
+const suma = a + b;
+const resta = a - b;
+const multiplicacion = a * c;
+
+console.log(`La suma de ${a} y ${b} es: ${suma}`);
+console.log(`La resta de ${a} y ${b} es: ${resta}`);
+console.log(`La multiplicación de ${a} y ${c} es: ${multiplicacion}`);
